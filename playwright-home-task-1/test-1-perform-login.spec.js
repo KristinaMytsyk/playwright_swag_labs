57ef3f93979f211188f1644ecb22386d3d5d1edb
Kristina Mytsyk <kristimitsik@gmail.com>
2024-11-20 23:31:00 +0200

    Code improvements

--- playwright-home-task-1/test-1-perform-login.spec.js
+++ playwright-home-task-1/test-1-perform-login.spec.js
@@ -1,26 +1,33 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+  await page.getByTestId('username').fill('standard_user');
+  await page.getByTestId('password').fill('secret_sauce');
+  await page.getByTestId('login-button').click();
+});
+
 test('Products title is displayed', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('[data-test="title"]')).toBeVisible();
+  // await page.goto('/');
+  // await page.getByTestId('username').fill('standard_user');
+  // await page.getByTestId('password').fill('secret_sauce');
+  // await page.getByTestId('login-button').click();
+  await expect(page.getByTestId('title')).toBeVisible();
 });
 
 test('Shopping Cart icon is displayed', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+    // await page.goto('/');
+    // await page.getByTestId('username').fill('standard_user');
+    // await page.getByTestId('password').fill('secret_sauce');
+    // await page.getByTestId('login-button').click();
     await expect(page.locator('#shopping_cart_container')).toBeVisible();
   });
 
 test('More than 1 product is displayed', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+    // await page.goto('/');
+    // await page.getByTestId('username').fill('standard_user');
+    // await page.getByTestId('password').fill('secret_sauce');
+    // await page.getByTestId('login-button').click();
     const products = await page.locator('.inventory_item');
     const productCount = await products.count();
     expect(productCount).toBeGreaterThan(1);
--- playwright-home-task-1/test-2-add-product-to-the-cart.spec.js
+++ playwright-home-task-1/test-2-add-product-to-the-cart.spec.js
@@ -1,39 +1,50 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+  await page.getByTestId('username').fill('standard_user');
+  await page.getByTestId('password').fill('secret_sauce');
+  await page.getByTestId('login-button').click();
+});
+
 test('Verify Shopping Cart icon contains the number of products added', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  const firstProduct = await page.locator('.inventory_list > *:first-child');
-  await firstProduct.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  // await page.goto('/');
+  // await page.getByTestId('username').fill('standard_user');
+  // await page.getByTestId('password').fill('secret_sauce');
+  // await page.getByTestId('login-button').click();
+  //const firstProduct = await page.locator('.inventory_list > *:first-child');
+  //PW method:
+  const firstProduct = await page.locator('.inventory_list').first();
+  await firstProduct.getByTestId('add-to-cart-sauce-labs-backpack').click();
   const shoppingCartBadge = await page.locator('span.shopping_cart_badge');
   await expect(shoppingCartBadge).toHaveText('1');
 });
 
 test('Verify the added product is displayed in the Shopping Cart', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+  // await page.goto('/');
+  // await page.getByTestId('username').fill('standard_user');
+  // await page.getByTestId('password').fill('secret_sauce');
+  // await page.getByTestId('login-button').click();
   const firstProduct = await page.locator('.inventory_list > *:first-child');
-  const firstProductName = await firstProduct.locator('[data-test="inventory-item-name"]').innerText();
-  await firstProduct.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  const cartProductName = await page.locator('[data-test="inventory-item-name"]').innerText();
-  expect(cartProductName).toBe(firstProductName);
+  const firstProductName = await firstProduct.getByTestId('inventory-item-name').innerText();
+  await firstProduct.getByTestId('add-to-cart-sauce-labs-backpack').click();
+  await page.getByTestId('shopping-cart-link').click();
+  //const cartProductName = await page.getByTestId('inventory-item-name').innerText();
+  //expect(cartProductName).toBe(firstProductName);
+  //Or we can use assertion that waits until condition is met (EugenePetrik)):
+  await expect(page.getByTestId('inventory-item-name')).toHaveText(firstProductName);
 });
 
 test('Verify no products are available in the Shopping Cart after clicking Remove', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+  // await page.goto('/');
+  // await page.getByTestId('username').fill('standard_user');
+  // await page.getByTestId('password').fill('secret_sauce');
+  // await page.getByTestId('login-button').click();
   const firstProduct = await page.locator('.inventory_list > *:first-child');
-  await firstProduct.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
-  const inventoryItem = page.locator('[data-test="inventory-item"]');
+  await firstProduct.getByTestId('add-to-cart-sauce-labs-backpack').click();
+  await page.getByTestId('shopping-cart-link').click();
+  await page.getByTestId('remove-sauce-labs-backpack').click();
+  const inventoryItem = page.getByTestId('inventory-item');
   await expect(inventoryItem).not.toBeVisible();
   await expect(inventoryItem).toHaveCount(0);
 });
